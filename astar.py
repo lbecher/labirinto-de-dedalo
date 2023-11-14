3268a529c41b4d026ae74dc746cf69c378f581e0
Vinicius Sendoski de Andrade <viniciussendoski@gmail.com>
2023-11-14 17:52:33 -0300

    if de posição da atual == final

--- astar.py
+++ astar.py
@@ -1,9 +1,9 @@
 from player import *
 from maze import *
 
-# inicia visinho, a posição e os valores de g,h,f
-def __init__(self, neighbor=None, position=None):
-        self.neighbor = neighbor
+# inicia os valores de g,h,f & define posição e parent
+def __init__(self, parent=None, position=None):
+        self.parent = parent
         self.position = position
 
         self.g = 0
@@ -33,7 +33,41 @@ def astar(pygame, maze_matrix):
     # Adiciona a lista a posção inicial do player A*
     open_list.append(player_astar)
 
-    while (end_maze != player_astar):
-        if end_maze == player_astar:
+    # Laço enquanto não achar o final, len pega o comprimento total da lista
+    while len(open_list) > 0:
+        #pega a posição atual na lista
+        current_position = open_list[0]
+        #inicializa com 0 o indice atual
+        current_index = 0
+        # utiliza enumerate para para obter o valor e o indice
+        for index, item in enumerate(open_list):
+            # verifica se o custo total de da posição inicial é menor que a final
+            if item.f < current_position.f:
+                current_position = item
+                current_index = index
+
+        # adiciona mais um item a lista a lista aberta, o novo melhor valor
+        # a lista fechada resebe a nova melher posição 
+        open_list.pop(current_index)
+        closed_list.append(current_position)
+
+        # verifica se a posição atual é a posição destino
+        if current_position == end_maze:
+            # inicia lista do caminho
+            path = []
+            # cria uma variavel para evitar que mudanças na execução alterem a lista
+            current = current_position
+
+            # percore o laço enquanto não for vazio
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+                # inverte a lista para retornala
+                return list(reversed(path))
+
+        # posição atual não é destino
+        else:
+            # lista para vizinhos 
+            neighbor = []
+            # laço que tenta gerar vizinhos nas 8 posições adjasentes 
             pass
-    return 0

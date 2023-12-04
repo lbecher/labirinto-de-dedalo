9266ac42759803f6ff309126f6576696baf29597
Vinicius Sendoski de Andrade <viniciussendoski@gmail.com>
2023-12-04 09:06:25 -0300

    full a*

--- a_star.py
+++ a_star.py
@@ -20,15 +20,36 @@ def min_value(list):
 
     return min_value
 
+# No labirinto o A* não pode andar na diagonal, mas da pra andar a diagonal normamente
+def get_movement(teste_movimento):
+    if teste_movimento:
+        movement = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, -1), (-1, 1)]
+    else:
+        movement = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    return movement
+
+# Permite selecionar qual metodo heuristuico utilizar
+def heuristic_calculation(nodo, end_maze, use_manhattan):
+    if use_manhattan == True:
+        return abs(nodo.position[0] - end_maze.position[0]) + abs(nodo.position[1] - end_maze.position[1])
+    else:
+        return ((nodo.position[0] - end_maze.position[0]) ** 2) + ((nodo.position[1] - end_maze.position[1]) ** 2)
+    
 
 def calculate_a_star(maze_matrix, end_i, end_j, player2_i, player2_j):
     # posição inicial do player e final da matriz
     player_astar = AStarNode(position=(player2_i, player2_j))
     end_maze = AStarNode(position=(end_i, end_j))
+    
+    # Escolhe forma de percorer percorer os nós adjacentes
+    adjacency_choice = False
+    movement = get_movement(adjacency_choice)
+
+    # Define se o calculo da heuristica sera por distancia Manhattan ou Euclidiana
+    heuristic = True    
 
     player_astar.g = 0
-    # formula para calcular a distancia Manhattan
-    player_astar.h = abs(player_astar.position[0] - end_maze.position[0]) + abs(player_astar.position[1] - end_maze.position[1])
+    player_astar.h = heuristic_calculation(player_astar, end_maze, heuristic)
     player_astar.f = player_astar.g + player_astar.h
 
     # faça uma lista aberta contendo apenas o nó inicial
@@ -38,13 +59,6 @@ def calculate_a_star(maze_matrix, end_i, end_j, player2_i, player2_j):
     # faça uma lista fechada vazia
     closed_list = []
     
-    # Exeção a regra do A* para que não se possa andar na diagonal no labirinto
-    teste_movimento = ASTAR_MOV
-    if teste_movimento == True:
-        movement = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, -1), (-1, 1)]
-    else:
-        movement = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
     # enquanto o nó de destino não for alcançado
     while open_list:
         # chama função de minimo de f 
@@ -72,7 +86,7 @@ def calculate_a_star(maze_matrix, end_i, end_j, player2_i, player2_j):
                 if 0 <= adjacent_position[0] < len(maze_matrix) and 0 <= adjacent_position[1] < len(maze_matrix[0]) and maze_matrix[adjacent_position[0]][adjacent_position[1]] != 0:
                     adjacent_node = AStarNode(position=adjacent_position)
                     adjacent_node.g = current_node.g + 1
-                    adjacent_node.h = abs(adjacent_node.position[0] - end_maze.position[0]) + abs (adjacent_node.position[1] - end_maze.position[1])
+                    adjacent_node.h = heuristic_calculation(adjacent_node, end_maze,heuristic)
                     adjacent_node.f = adjacent_node.g + adjacent_node.h
 
                     # Verifica se algum nó adjacent_node esta na lista fechada
